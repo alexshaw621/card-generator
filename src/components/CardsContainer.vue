dc480e7f5ed67341336efbd4308a71f126aed52c
alexshaw621 <alexshaw621@gmail.com>
2022-06-09 14:47:47 -0400

    styling updates

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -27,27 +27,39 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <h1>Card Generator</h1>
-    <form class="bg-white flex items-center" @submit="onSubmit">
+  <div class="container mx-auto">
+    <h1 class="text-center md:text-left">Card Generator</h1>
+    <form
+      class="bg-white flex flex-col md:flex-row items-start md:items-center"
+      @submit="onSubmit"
+      data-generator-form
+    >
       <div class="flex-1 flex items-center flex-col md:flex-row">
         <div class="flex items-center">
           <span>Genreate</span>
-          <input type="number" max="5" v-model="formData.count" required />
-          <span>random cards,</span>
+          <input type="number" min="1" v-model="formData.count" required />
+          <span>random cards,&nbsp;</span>
         </div>
-        <div class="flex items-center">
+        <div class="flex items-center mt-3 md:mt-0">
           <span>each with</span>
-          <input type="number" max="5" v-model="formData.dimension" required />
+          <input
+            type="number"
+            min="1"
+            max="5"
+            v-model="formData.dimension"
+            required
+          />
           <span>rows/columns.</span>
         </div>
       </div>
-      <div>
-        <button class="button" type="submit">Genreate</button>
+      <div class="w-full md:w-auto mt-3 md:mt-0">
+        <button class="inline-block button w-full md:w-auto" type="submit">
+          Genreate
+        </button>
       </div>
     </form>
 
-    <div>
+    <div class="flex flex-wrap justify-center mt-8">
       <div v-for="n in gridData.count" :key="n">
         <RandomCard :dimension="gridData.dimension" />
       </div>
