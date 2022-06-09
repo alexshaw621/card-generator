7f1817a878fbe66a291b925c40963472bdabe774
alexshaw621 <alexshaw621@gmail.com>
2022-06-09 12:23:26 -0400

    initial commit

---
+++ src/components/RandomCard.vue
@@ -0,0 +1,9 @@
+import Vue from 'vue'; export default Vue.extend({ props: { rows: { type:
+Number, default: 5 }, cols: { type: Number, default: 5 }, }, data() { return {
+avaailableCharacters: `ABCDEFGHIJKLMNOPQRSTUVWXYZ`, }; }, computed: {
+totalCount(): number { return this.rows * this.clos; }, shuffledCharacters():
+string[] { let characters = this.avaailableCharacters.split(""); let length =
+characters.length; for(let i = length - 1; i > 0; i--) { let j =
+Math.floor(Math.random() * (i + 1)); let tmp = characters[i]; characters[i] =
+characters[j]; characters[j] = tmp; } return characters; }, }, mounted(): void {
+}, methods: { }, });
