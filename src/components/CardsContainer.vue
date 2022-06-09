6c2177f67936784dcae4bea2fa2b4db24babd448
alexshaw621 <alexshaw621@gmail.com>
2022-06-09 14:23:55 -0400

    add components

---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,56 @@
+<script>
+import RandomCard from './RandomCard.vue';
+
+export default {
+  components: {
+    RandomCard,
+  },
+  data() {
+    return {
+      formData: {
+        count: { type: Number },
+        dimension: { type: Number },
+      },
+      gridData: {
+        count: 0,
+        dimension: 0,
+      },
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      this.gridData = { ...this.formData };
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>Card Generator</h1>
+    <form class="bg-white flex items-center" @submit="onSubmit">
+      <div class="flex-1 flex items-center flex-col md:flex-row">
+        <div class="flex items-center">
+          <span>Genreate</span>
+          <input type="number" max="5" v-model="formData.count" required />
+          <span>random cards,</span>
+        </div>
+        <div class="flex items-center">
+          <span>each with</span>
+          <input type="number" max="5" v-model="formData.dimension" required />
+          <span>rows/columns.</span>
+        </div>
+      </div>
+      <div>
+        <button class="button" type="submit">Genreate</button>
+      </div>
+    </form>
+
+    <div>
+      <div v-for="n in gridData.count" :key="n">
+        <RandomCard :dimension="gridData.dimension" />
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/RandomCard.vue
+++ src/components/RandomCard.vue
@@ -1,9 +1,44 @@
-import Vue from 'vue'; export default Vue.extend({ props: { rows: { type:
-Number, default: 5 }, cols: { type: Number, default: 5 }, }, data() { return {
-avaailableCharacters: `ABCDEFGHIJKLMNOPQRSTUVWXYZ`, }; }, computed: {
-totalCount(): number { return this.rows * this.clos; }, shuffledCharacters():
-string[] { let characters = this.avaailableCharacters.split(""); let length =
-characters.length; for(let i = length - 1; i > 0; i--) { let j =
-Math.floor(Math.random() * (i + 1)); let tmp = characters[i]; characters[i] =
-characters[j]; characters[j] = tmp; } return characters; }, }, mounted(): void {
-}, methods: { }, });
+<script>
+export default {
+  props: {
+    dimension: { type: Number, default: 5 },
+  },
+  data() {
+    return {
+      availableLetters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      backupClasses:
+        'grid-cols-1 grid-cols-2 grid-cols-3 grid-cols-4 grid-cols-5',
+    };
+  },
+  computed: {
+    totalCount() {
+      return Math.pow(this.dimension, 2);
+    },
+    className() {
+      return `inline-grid gap-3 grid-cols-${this.dimension}`;
+    },
+    shuffledLetters() {
+      let letters = this.availableLetters.split('');
+      const length = letters.length;
+
+      for (let i = length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = letters[i];
+        letters[i] = letters[j];
+        letters[j] = temp;
+      }
+
+      return letters.slice(0, this.totalCount);
+    },
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <div :class="className" :data-dimension="dimension" data-card>
+    <span v-for="letter in shuffledLetters" :key="letter" data-card-letter>
+      {{ letter }}
+    </span>
+  </div>
+</template>
